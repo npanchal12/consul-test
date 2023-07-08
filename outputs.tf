cfeb409c0831be6aeb24fcf97d43a42faecaf986
Nimesh Panchal <npanchal@Nimeshs-MacBook-Pro.local>
2023-07-08 15:36:39 +0800

    remove consult cluster

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
-output "consul_root_token" {
-  value     = hcp_consul_cluster_root_token.token.secret_id
-  sensitive = true
-}
+# output "consul_root_token" {
+#   value     = hcp_consul_cluster_root_token.token.secret_id
+#   sensitive = true
+# }
 
-output "consul_url" {
-  value = hcp_consul_cluster.main.public_endpoint ? (
-    hcp_consul_cluster.main.consul_public_endpoint_url
-    ) : (
-    hcp_consul_cluster.main.consul_private_endpoint_url
-  )
-}
+# output "consul_url" {
+#   value = hcp_consul_cluster.main.public_endpoint ? (
+#     hcp_consul_cluster.main.consul_public_endpoint_url
+#     ) : (
+#     hcp_consul_cluster.main.consul_private_endpoint_url
+#   )
+# }
 
-output "kubeconfig_filename" {
-  value = abspath(one(module.eks[*].kubeconfig_filename))
-}
+# output "kubeconfig_filename" {
+#   value = abspath(one(module.eks[*].kubeconfig_filename))
+# }
 
 # output "helm_values_filename" {
 #   value = abspath(module.eks_consul_client.helm_values_file)
