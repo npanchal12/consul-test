3642f0fc5aacc0f9624511d3fde1f998b5c787d6
Nimesh Panchal <npanchal@Nimeshs-MacBook-Pro.local>
2023-07-08 14:16:30 +0800

    remove unwanted resources

--- main.tf
+++ main.tf
@@ -1,26 +1,26 @@
-# module "vpc" {
-#   source  = "terraform-aws-modules/vpc/aws"
-#   version = "2.78.0"
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.78.0"
 
-#   name                 = "${local.cluster_id}-vpc"
-#   cidr                 = "10.0.0.0/16"
-#   azs                  = data.aws_availability_zones.available.names
-#   public_subnets       = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-#   private_subnets      = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-#   enable_nat_gateway   = true
-#   single_nat_gateway   = true
-#   enable_dns_hostnames = true
+  name                 = "${local.cluster_id}-vpc"
+  cidr                 = "10.0.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
+  public_subnets       = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  private_subnets      = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+}
+
+# data "aws_eks_cluster" "cluster" {
+#   count = local.install_eks_cluster ? 1 : 0
+#   name  = module.eks[0].cluster_id
 # }
 
-data "aws_eks_cluster" "cluster" {
-  count = local.install_eks_cluster ? 1 : 0
-  name  = module.eks[0].cluster_id
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  count = local.install_eks_cluster ? 1 : 0
-  name  = module.eks[0].cluster_id
-}
+# data "aws_eks_cluster_auth" "cluster" {
+#   count = local.install_eks_cluster ? 1 : 0
+#   name  = module.eks[0].cluster_id
+# }
 
 # module "eks" {
 #   count                  = local.install_eks_cluster ? 1 : 0
