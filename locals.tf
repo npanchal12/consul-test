a53bfbf66113b8804fdc387a4500c7e2dab55fd5
Nimesh Panchal <npanchal@Nimeshs-MacBook-Pro.local>
2023-07-08 14:19:28 +0800

    remove unwanted resources

--- locals.tf
+++ locals.tf
@@ -1,8 +1,8 @@
-# locals {
-#   vpc_region          = "ap-southeast-1"
+locals {
+  vpc_region          = "ap-southeast-1"
 #   hvn_region          = "ap-southeast-1"
-#   cluster_id          = "consul-cluster"
+  cluster_id          = "consul-cluster"
 #   hvn_id              = "consul-hvn"
 #   install_eks_cluster = true
 #   install_demo_app    = true
-# }
+}
