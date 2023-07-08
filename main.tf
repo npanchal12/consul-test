09580bfa1118d83be0b9098a246e6fee8d2202c5
Nimesh Panchal <npanchal@Nimeshs-MacBook-Pro.local>
2023-07-08 16:25:47 +0800

    add vpc resoruces

--- main.tf
+++ main.tf
@@ -1,50 +1,50 @@
-# module "vpc" {
-#   source  = "terraform-aws-modules/vpc/aws"
-#   version = "2.78.0"
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.78.0"
 
-#   name                 = "${local.cluster_id}-vpc"
-#   cidr                 = "10.0.0.0/16"
-#   azs                  = data.aws_availability_zones.available.names
-#   public_subnets       = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-#   private_subnets      = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-#   enable_nat_gateway   = true
-#   single_nat_gateway   = true
-#   enable_dns_hostnames = true
-# }
+  name                 = "${local.cluster_id}-vpc"
+  cidr                 = "10.0.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
+  public_subnets       = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  private_subnets      = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+}
 
-# data "aws_eks_cluster" "cluster" {
-#   count = local.install_eks_cluster ? 1 : 0
-#   name  = module.eks[0].cluster_id
-# }
+data "aws_eks_cluster" "cluster" {
+  count = local.install_eks_cluster ? 1 : 0
+  name  = module.eks[0].cluster_id
+}
 
-# data "aws_eks_cluster_auth" "cluster" {
-#   count = local.install_eks_cluster ? 1 : 0
-#   name  = module.eks[0].cluster_id
-# }
+data "aws_eks_cluster_auth" "cluster" {
+  count = local.install_eks_cluster ? 1 : 0
+  name  = module.eks[0].cluster_id
+}
 
-# module "eks" {
-#   count                  = local.install_eks_cluster ? 1 : 0
-#   source                 = "terraform-aws-modules/eks/aws"
-#   version                = "17.24.0"
-#   kubeconfig_api_version = "client.authentication.k8s.io/v1beta1"
+module "eks" {
+  count                  = local.install_eks_cluster ? 1 : 0
+  source                 = "terraform-aws-modules/eks/aws"
+  version                = "17.24.0"
+  kubeconfig_api_version = "client.authentication.k8s.io/v1beta1"
 
-#   cluster_name    = "${local.cluster_id}-eks"
-#   cluster_version = "1.27"
-#   subnets         = module.vpc.private_subnets
-#   vpc_id          = module.vpc.vpc_id
+  cluster_name    = "${local.cluster_id}-eks"
+  cluster_version = "1.27"
+  subnets         = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
 
-#   manage_aws_auth = false
+  manage_aws_auth = false
 
-#   node_groups = {
-#     application = {
-#       name_prefix      = "hashicups"
-#       instance_types   = ["t3a.medium"]
-#       desired_capacity = 3
-#       max_capacity     = 3
-#       min_capacity     = 3
-#     }
-#   }
-# }
+  node_groups = {
+    application = {
+      name_prefix      = "hashicups"
+      instance_types   = ["t3a.medium"]
+      desired_capacity = 3
+      max_capacity     = 3
+      min_capacity     = 3
+    }
+  }
+}
 
 # # The HVN created in HCP
 # resource "hcp_hvn" "main" {
