432b857e5c19a3ffd3a65ce6b8944d86460547f1
Nimesh Panchal <npanchal@Nimeshs-MacBook-Pro.local>
2023-07-09 13:43:32 +0800

    add exec for helm

--- providers.tf
+++ providers.tf
@@ -44,7 +44,7 @@ provider "helm" {
     token                  = local.install_eks_cluster ? data.aws_eks_cluster_auth.cluster[0].token : ""
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", local.cluster_name]
+      args        = ["eks", "get-token", "--cluster-name", local.cluster_id]
       command     = "aws"
     }
   }
